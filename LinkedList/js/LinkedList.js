6f531d9ffc4d989ed9c4bd785459c144131dedb0
trainingmontage <nemo.omen@gmail.com>
2021-11-23 17:47:10 -0600

    Start js LinkedList

--- LinkedList/js/LinkedList.js
+++ LinkedList/js/LinkedList.js
@@ -1,10 +1,60 @@
-class LinkedList {
-  head;
-  length;
-  constructor() {
-    this.head = undefined;
-    this.length = 0;
+export class ListNode {
+  next;
+  data;
+  constructor(val) {
+    this.data = val;
+    this.next = undefined;
   }
 }
 
-export default LinkedList;
+export class LinkedList {
+  head;
+  tail;
+  #length;
+  constructor() {
+    this.head = undefined;
+    this.tail = undefined;
+    this.length = 0;
+  }
+
+  size() {
+    return this.length;
+  }
+
+  push(val) {
+    const node = new ListNode(val);
+
+    if(!this.head) {
+      this.head = node;
+      this.tail = this.head;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.length++;
+    console.log(this.tail);
+    return this.tail;
+  }
+
+  pop() {
+    if(!list.head) return;
+    let current = this.head;
+    let newTail = current;
+
+    while(current.next) {
+      newTail = current;
+      current = current.next;
+    }
+
+    this.tail = newTail;
+    this.tail.next = undefined;
+    this.length--;
+
+    if(this.length === 0) {
+      this.head = undefined;
+      this.tail = undefined;
+    }
+
+
+  }
+}
--- LinkedList/js/test.js
+++ LinkedList/js/test.js
@@ -1,7 +1,40 @@
-import {assert, assertExists, assertObjectMatch} from "https://deno.land/std/testing/asserts.ts";
-import LinkedList from './LinkedList.js';
+import {assert, assertExists, assertEquals, assertObjectMatch} from "https://deno.land/std/testing/asserts.ts";
+import chai from 'https://cdn.skypack.dev/chai';
 
-Deno.test("Test LinkedList was instantiated", () => {
+import { LinkedList, ListNode } from './LinkedList.js';
+
+Deno.test("LinkedList was instantiated", () => {
   const list = new LinkedList();
   assertObjectMatch(list, {head: undefined, length: 0});
+});
+
+Deno.test("Pushed ListNode should be instance of ListNode", () => {
+  const list = new LinkedList();
+  const pushedNode = list.push('First');
+  assert(pushedNode instanceof ListNode);
+});
+
+Deno.test("Pushed ListNode data should be 'First'", () => {
+  const list = new LinkedList();
+  const pushedNode = list.push('First');
+  assertEquals(pushedNode.data, 'First');
+});
+
+Deno.test("Pushed ListNode next should be undefined", () => {
+  const list = new LinkedList();
+  const pushedNode = list.push('First');
+  assertEquals(pushedNode.next, undefined);
+});
+
+Deno.test("List size should be 1 after first push", () => {
+  const list = new LinkedList();
+  const pushedNode = list.push('First');
+  assertEquals(list.size(), 1);
+});
+
+Deno.test("Removal of only element should result in list size of 0", () => {
+    const list = new LinkedList();
+    const pushedNode = list.push('First');
+    list.pop();
+    assertEquals(list.size(), 0);
 });
